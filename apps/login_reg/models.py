bc925535cb964650fe5ce64cded3dacb4c98a028
Erick Smith <smith.s.erick@gmail.com>
2017-10-25 15:05:22 -0700

    login-reg added with user class

--- apps/login_reg/models.py
+++ apps/login_reg/models.py
@@ -1,6 +1,47 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+from django.contrib.auth.hashers import make_password, check_password
+from django.contrib.auth.password_validation import validate_password
+import re
+EMAIL_REGEX = re.compile(r'^[a-zA-Z0-9.+_-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]+$')
 
 # Create your models here.
+class UserManager(models.Manager):
+    def validator(self, postData):
+        errors = {
+            'error': []
+        }
+        if len(postData['first_name']) < 2 or len(postData['last_name']) < 2:
+            errors['error'].append('Your first and last name must be longer than 1 character')
+        if postData['password'] != postData['confirm_password']:
+            errors['error'].append('Your password does not match the confim password')
+        if EMAIL_REGEX.match(postData['email']) is None:
+            errors['error'].append('Invalid email format')
+        if User.objects.filter(email=postData['email']):
+            errors['error'].append('Email already registered')
+        else:
+            errors['error'].append('Please enter in your birthday')
+        try:
+            validate_password(postData['password'])
+        except Exception as e:
+            errors['password'] = e
+        return errors
+
+    def password_hasher(self, pw):
+        hashed_password = make_password(pw)
+        return hashed_password
+
+    def password_checker(self, pw, encoded_pw):
+        return check_password(pw, encoded_pw)
+
+#DB models
+class User(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField(max_length=75)
+    password = models.CharField(max_length=255)
+    isAdmin = models.BooleanField(default=False)
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    objects = UserManager()
--- apps/login_reg/views.py
+++ apps/login_reg/views.py
@@ -1,8 +1,45 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.shortcuts import render, redirect
+from .models import User
+from django.contrib import messages
 
 # Create your views here.
 def sign_in(request):
     return render(request, 'login_reg/sign-in.html')
+
+
+
+#login-reg-logout managers
+def register(request):
+    if request.method == 'POST':
+        errors = User.objects.validator(request.POST)
+        if errors.get('password') is not None or errors['error']:
+            if errors['error']:
+                for error in errors['error']:
+                    messages.error(request, error)
+            if errors.get('password') is not None:
+                for error in errors['password']:
+                    messages.error(request, error)
+            return redirect#(####FILL IN###)
+    hashed_password = User.objects.password_hasher(request.POST['password'])
+    User.objects.create(name=request.POST['name'], alias=request.POST['alias'], email=request.POST['email'], birthday=request.POST['birthday'], password=hashed_password)
+    request.session['id'] = User.objects.get(email=request.POST['email']).id
+    return redirect#(####FILL IN###)
+
+def login(request):
+    if request.method == 'POST':
+        user = User.objects.filter(email=request.POST['email'])
+        if user:
+            if User.objects.password_checker(request.POST['password'], user[0].password):
+                request.session['id'] = user[0].id
+                return redirect#(####FILL IN###)
+            else:
+                return redirect#(####FILL IN###)
+        else:
+            messages.warning(request, 'Email did not match password')
+            return redirect#(####FILL IN###)
+
+def logout(request):
+    del request.session['id']
+    return redirect#(####FILL IN###)
