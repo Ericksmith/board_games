87b498f16d4140910a9914b6e39929290db1488a
Erick Smith <smith.s.erick@gmail.com>
2017-10-25 17:54:40 -0700

    migrated games models

--- apps/admin/views.py
+++ apps/admin/views.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
 from django.shortcuts import render, redirect
 # from login_reg.models import User
 import requests
@@ -50,18 +51,35 @@ def searchProducts(request):
         return redirect(addProduct)
 
 def select_game(request, game_id):
-    selected_game = requests.get("https://www.boardgamegeek.com/xmlapi/boardgame/{}".format(game_id))
-    game = ET.fromstring(selected_game.content)
+    resp = requests.get("https://bgg-json.azurewebsites.net/thing/{}".format(game_id))
+    game = json.loads(resp.text)
+    print(game)
     game_to_add = {
-        'playtime': game[0].find('playingtime').text,
-        'minplayers': game[0].find('minplayers').text,
-        'maxplayers': game[0].find('maxplayers').text,
-        'description': game[0].find('description').text,
-        'thumbnail': game[0].find('thumbnail').text,
-        'image': game[0].find('image').text,
-        'yearpublished': game[0].find('yearpublished').text,
-        'title': game[0].find('name').text,
-        'publisher': game[0].find('boardgamepublisher').text,
+        'playtime': game['playingTime'],
+        'minplayers': game['minPlayers'],
+        'maxplayers': game['maxPlayers'],
+        'description': game['description'],
+        'thumbnail': game['thumbnail'],
+        'image': game['image'],
+        'yearpublished': game['yearPublished'],
+        'title': game['name'],
+        'publisher': game['publishers'][0],
+        'rating': game['bggRating'],
+        'rank': game['rank']
     }
+
+    # selected_game = requests.get("https://www.boardgamegeek.com/xmlapi/boardgame/{}".format(game_id))
+    # game = ET.fromstring(selected_game.content)
+    # game_to_add = {
+    #     'playtime': game[0].find('playingtime').text,
+    #     'minplayers': game[0].find('minplayers').text,
+    #     'maxplayers': game[0].find('maxplayers').text,
+    #     'description': game[0].find('description').text,
+    #     'thumbnail': game[0].find('thumbnail').text,
+    #     'image': game[0].find('image').text,
+    #     'yearpublished': game[0].find('yearpublished').text,
+    #     'title': game[0].find('name').text,
+    #     'publisher': game[0].find('boardgamepublisher').text,
+    # }
     request.session['selected_game'] = game_to_add
     return redirect(addProduct)
