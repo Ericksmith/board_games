a1f7914b0ba64c6c55c28a9ee81fbab26a2c2b0d
Chris Konopasek <cbkonopasek@gmail.com>
2017-10-26 18:19:36 -0700

    added logic for main page subgroups

--- board_games/urls.py
+++ board_games/urls.py
@@ -17,6 +17,7 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
+    url(r'^$', include('apps.store.urls')),
     url(r'^admin/', include('apps.admin.urls')),
     url(r'^sign-in', include('apps.login_reg.urls')),
     # url(r'^', include('apps.store.urls')),
