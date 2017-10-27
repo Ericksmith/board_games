40d0ad472bbb7eea373dcacec47c6ef4e7c62234
Chris Konopasek <cbkonopasek@gmail.com>
2017-10-27 16:13:13 -0700

    bug fixes for all pages

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -2,7 +2,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
+    # pages
     url(r'^$', views.index),
     url(r'^results', views.results),
-    url(r'^game/(?P<num>\d)$', views.game)
+    url(r'^game/(?P<num>\d)$', views.game),
+
+    # functions
+    url(r'^add_to_cart', views.add_to_cart),
+
 ]
